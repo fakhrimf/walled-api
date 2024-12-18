29ee9d489a9325985b02c7e7964e508a619b879c
Fakhri Maulana Falah <fakhrimf23@gmail.com>
2024-12-18 15:05:54 +0700

    fix: bcrypt hotfix

--- services/users.service.js
+++ services/users.service.js
@@ -1,5 +1,5 @@
 const repo = require("../repositories/users.repository")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const { genToken } = require("../utils/auth.util")
 
 const createUser = async (user) => {
