d0a5ee377cde2a4aa4302b3e50e1f76a5865f653
Fakhri Maulana Falah <fakhrimf23@gmail.com>
2024-12-18 14:19:10 +0700

    add: add vercel integration

--- db/db.js
+++ db/db.js
@@ -1,9 +1,5 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'walled_db',
-    password: 'admin',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL
 })
 module.exports = pool
